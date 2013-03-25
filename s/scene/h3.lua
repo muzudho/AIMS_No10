6e7eb6aa18491f0401378bf9d79d5260d2eb2770
鬼畜大王 <kichikudaiou@gmail.com>
2013-03-26 00:14:39 +0900

    h3

--- s/scene/h3.lua
+++ s/scene/h3.lua
@@ -8,6 +8,11 @@ function h3_OnStart ()
 	-- 3.内容
 	data = { "Ａ", "Ｂ", "Ｃ", "Ｄ", "Ｅ", "Ｆ", "Ｇ",
 			 "Ｈ", "Ｉ", "Ｊ", "Ｋ", "Ｌ", "Ｍ", "Ｎ" }
+	-- 4.欄の表示位置
+	viewX     = 16  * 32
+	viewY     =  4  * 32
+	viewWidth =  5  * 32
+	viewHeight=  4  * 32
 
 	-- 1.まず画像を読み込む
 	G.background   = loadGraphic("gfx/Bg_h3.png");
@@ -20,7 +25,7 @@ function h3_OnStart ()
 	--     レイヤー番号は0から11までの12枚で、数字が大きいほど上になります。
 	--     後から追加したシーンは、前に追加したシーンよりも上になります。
 	A.background   = createActor(G.background, 640/2,   480/2,  8);
-	A.frame1       = createActor(G.frame1    , 640/2, 6* 32  , 11);
+	A.frame1       = createActor(G.frame1    , viewX, viewY  , 11);
 	
 	-- 2.5.アクターに 9patch をセットします。
    	--タイトルメニュー
@@ -29,17 +34,17 @@ function h3_OnStart ()
 	--     cut9PatchGraphic2(        g, x, y,  w,  h, w_left, h_top, w_right, h_bottom)
 	tbl1 = cut9PatchGraphic2( G.frame1, 0, 0, 64, 64,      8,     8,       8,        8)
 	set9patchGraphic( A.frame1, tbl1 )
-   	addMover( A.frame1, -1, 50, MOVER_SETZOOM, 176, 200 )
+   	addMover( A.frame1, -1, 50, MOVER_SETZOOM, viewWidth, viewHeight )
    	
-	A.cursor1= createTextActor( F.font1, "↓"   ,  8*32,  4*32  , 11 );
-	A.msg1   = createTextActor( F.font1, data[1],  8*32,  5*32  , 11 );
-	A.msg2   = createTextActor( F.font1, data[2],  9*32,  5*32  , 11 );
-	A.msg3   = createTextActor( F.font1, data[3], 10*32,  5*32  , 11 );
-	A.msg4   = createTextActor( F.font1, data[4], 11*32,  5*32  , 11 );
+	A.cursor1= createTextActor( F.font1, "↓"   , viewX-2*32,  viewY-1.5*32  , 11 );
+	A.msg1   = createTextActor( F.font1, data[1], viewX-2*32,  viewY-0.5*32  , 11 );
+	A.msg2   = createTextActor( F.font1, data[2], viewX-1*32,  viewY-0.5*32  , 11 );
+	A.msg3   = createTextActor( F.font1, data[3], viewX+0*32,  viewY-0.5*32  , 11 );
+	A.msg4   = createTextActor( F.font1, data[4], viewX+1*32,  viewY-0.5*32  , 11 );
 	--ページ数
 	str = currentPage( 1, VIEW_LAST, viewCursor+viewHead ) .."／".. totalPage( VIEW_LAST, #data)
 	strw= getTextWidth(F.font1, str, false);
-	A.msg5   = createTextActor( F.font1, str    , 640/2-strw/2,  6*32  , 11 );
+	A.msg5   = createTextActor( F.font1, str    , viewX-strw/2,  viewY+0.5*32  , 11 );
 end
 
 function h3_OnStep ()
@@ -97,7 +102,7 @@ function h3_OnStep ()
 	-- 2.アクター動作部
 
     if( flg_Cursor==1 )then
-    	addMover( A.cursor1, -1, 1, MOVER_SETPOSITION, (viewCursor+8)*32, 4*32 )
+    	addMover( A.cursor1, -1, 1, MOVER_SETPOSITION, viewX-2*32+(viewCursor)*32, viewY-1.5*32 )
     end
 
     if( flg_Head==1 )then
@@ -105,28 +110,28 @@ function h3_OnStep ()
 			vanish(A.msg1);
 		end
 		if( 1 <= viewHead+1 and viewHead+1 <= #data)then
-			A.msg1   = createTextActor( F.font1, data[viewHead+1],  8*32, 5*32, 11 );
+			A.msg1   = createTextActor( F.font1, data[viewHead+1], viewX-2*32, viewY-0.5*32, 11 );
 		end
 
 		if(isAlive(A.msg2))then
 			vanish(A.msg2);
 		end
 		if( 1 <= viewHead+2 and viewHead+2 <= #data)then
-			A.msg2   = createTextActor( F.font1, data[viewHead+2],  9*32, 5*32, 11 );
+			A.msg2   = createTextActor( F.font1, data[viewHead+2], viewX-1*32, viewY-0.5*32, 11 );
 		end
 		
 		if(isAlive(A.msg3))then
 			vanish(A.msg3);
 		end
 		if( 1 <= viewHead+3 and viewHead+3 <= #data)then
-			A.msg3   = createTextActor( F.font1, data[viewHead+3], 10*32, 5*32, 11 );
+			A.msg3   = createTextActor( F.font1, data[viewHead+3], viewX+0*32, viewY-0.5*32, 11 );
 		end
 		
 		if(isAlive(A.msg4))then
 			vanish(A.msg4);
 		end
 		if( 1 <= viewHead+4 and viewHead+4 <= #data)then
-			A.msg4   = createTextActor( F.font1, data[viewHead+4], 11*32, 5*32, 11 );
+			A.msg4   = createTextActor( F.font1, data[viewHead+4], viewX+1*32, viewY-0.5*32, 11 );
 		end
 		
 		--ページ数
@@ -135,7 +140,7 @@ function h3_OnStep ()
 		end
 		str = currentPage( 1, VIEW_LAST, viewCursor+viewHead ) .."／".. totalPage( VIEW_LAST, #data)
 		strw= getTextWidth(F.font1, str, false);
-		A.msg5   = createTextActor( F.font1, str    , 640/2-strw/2,  6*32  , 11 );
+		A.msg5   = createTextActor( F.font1, str    , viewX-strw/2,  viewY+0.5*32  , 11 );
     end
     
     ::endFunc::
